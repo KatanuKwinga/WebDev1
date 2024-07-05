54750c2d1f02690d436186310c5f54869aeb6a00
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-05 14:17:27 +0300

    add new php

--- About.php
+++ About.php
@@ -10,30 +10,7 @@
     </head>
     <body>
         <!--Header section starts-->
-        <header class="header">
-            <!--Code for bakery logo and name in the navbar-->
-            <i class="fa-solid fa-cake-candles fa-3x" style="color: #d3ad7f;"></i>
-            <h1 class="name" style="color: #fff;text-align: left;">THE<span> LITTLE</span> BAKERY</h1>
-            <nav class="navbar">
-                <!--Code for the different links to other pages in the navbar-->
-                <a href="index.html">Home</a>
-                <a href="About.html">About Us</a>
-                <a href="Menu.html">Menu</a>
-                <a href="products.html">Our Products</a>
-                <a href="Reviews.html">Reviews</a>
-                <a href="contact.html">Contact Us</a>
-                <div class="dropdown-container">
-                    <!--Dropdown menu from the link 'More For You'-->
-                    <a href="#" class="dropdown-toggle">More For You</a>
-                    <ul class="dropdown">
-                        <!--Icons and links to the different pages in the dropdown menu-->
-                        <li><i class="fa-solid fa-right-to-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="login.html">Log In</a></li>
-                        <li><i class="fa-solid fa-user fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="signup.html">Sign Up</a></li>
-                        <li><i class="fa-solid fa-right-from-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="#">Log Out</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <?php include_once("Templates/nav.php"); ?>
         <!--Header section ends-->
         <!--About section starts-->
         <h1 class="title">ABOUT US</h1> <!--Title-->
---
+++ Templates/nav.php
@@ -0,0 +1,24 @@
+<header class="header">
+            <!--Code for bakery logo and name in the navbar-->
+            <i class="fa-solid fa-cake-candles fa-3x" style="color: #d3ad7f;"></i>
+            <h1 class="name" style="color: #fff;text-align: left;">THE<span> LITTLE</span> BAKERY</h1>
+            <nav class="navbar">
+                <!--Code for the different links to other pages in the navbar-->
+                <a href="index.php">Home</a>
+                <a href="About.php">About Us</a>
+                <a href="Menu.php">Menu</a>
+                <a href="products.php">Our Products</a>
+                <a href="Reviews.php">Reviews</a>
+                <a href="contact.php">Contact Us</a>
+                <div class="dropdown-container">
+                    <!--Dropdown menu from the link 'More For You'-->
+                    <a href="#" class="dropdown-toggle">More For You</a>
+                    <ul class="dropdown">
+                        <!--Icons and links to the different pages in the dropdown menu-->
+                        <li><i class="fa-solid fa-right-to-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="login.html">Log In</a></li>
+                        <li><i class="fa-solid fa-user fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="signup.html">Sign Up</a></li>
+                        <li><i class="fa-solid fa-right-from-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="#">Log Out</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
