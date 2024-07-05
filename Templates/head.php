ea7a804ac2232d3c34606075a23771595c147a2e
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-05 15:33:38 +0300

    added details to php files

---
+++ Templates/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8"/>
+        <title>MyWebsite</title>
+       <!-- custom css file link -->
+        <link rel="stylesheet" href="style.css">
+        <!--Link to the page for icons used in the code-->
+        <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
+    </head>
+    <body>
--- contact.php
+++ contact.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "UTF-8"/>
-        <title>MyWebsite</title>
-       <!-- custom css file link -->
-        <link rel="stylesheet" href="style.css">
-        <!--Link to the page for icons used in the code-->
-        <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
-    </head>
-    <body>
+<?php require_once("includes/db_connect.php"); ?>
+
         <!--Header section starts-->
         <?php include_once("Templates/nav.php"); ?>
         <!--Header section ends-->
