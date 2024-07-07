3230012be90cf6521aadc9f3a850ccb6875fa8c2
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-07 08:25:29 +0300

    added php link for header template to home page

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset = "UTF-8"/>
-        <title>MyWebsite</title>
-       <!-- custom css file link -->
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="index.css">
-        <!--Link to the page for icons used in the code-->
-        <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
-    </head>
+    <?php include_once("Templates/head.php"); ?>
     <body>
         <!--Header section starts-->
         <?php include_once("Templates/nav.php"); ?>
