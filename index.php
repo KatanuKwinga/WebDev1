43e76a4c05c9dd854b69ce3a5dbb3897157b224d
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-07 08:00:09 +0300

    added link to css for home page to the home page

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <title>MyWebsite</title>
        <!-- custom css file link -->
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="index.css">
         <!--Link to the page for icons used in the code-->
         <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
     </head>
