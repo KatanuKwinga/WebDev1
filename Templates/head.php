aa2642bf6865b9fe64cc7f44059460df0b50ce04
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-07 13:22:51 +0300

    fixed reference link to the css file

--- Templates/head.php
+++ Templates/head.php
@@ -4,7 +4,7 @@
         <meta charset = "UTF-8"/>
         <title>MyWebsite</title>
        <!-- custom css file link -->
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
         <!--Link to the page for icons used in the code-->
         <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
     </head>
