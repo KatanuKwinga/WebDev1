3dfa20a4cfb836c444556a357f55ebdb86a8c1ba
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-07 08:24:09 +0300

    edited php file for the header

--- Templates/head.php
+++ Templates/head.php
@@ -8,4 +8,4 @@
         <!--Link to the page for icons used in the code-->
         <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
     </head>
-    <body>
+    
