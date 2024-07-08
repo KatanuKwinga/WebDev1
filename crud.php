2315a63af59ad1f65cf590a481b777bf1be80b8c
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-08 22:42:13 +0300

    added navbar code

--- crud.php
+++ crud.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "UTF-8"/>
-        <title>MyWebsite</title>
-       <!-- custom css file link -->
-        <link rel="stylesheet" href="css/crud.css">
-        <!--Link to the page for icons used in the code-->
-        <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
-    </head>
- </html>
-<?php
 
+<?php
 // connection.php content
 $servername = "localhost";
 $username = "root";
@@ -87,18 +76,48 @@ if (isset($_POST['delete'])) {
 $conn->close();
 ?>
 
+<!--Beginning of html code for the page-->
 <!DOCTYPE html>
 <html>
-<head>
-    <title>CRUD Application</title>
-    <!-- Link to the external CSS file for styling -->
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
+    <head>
+        <meta charset = "UTF-8"/>
+        <title>MyWebsite</title>
+       <!-- custom css file link -->
+        <link rel="stylesheet" href="css/crud.css">
+        <!--Link to the page for icons used in the code-->
+        <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
+    </head>
+ </html>
 <body>
-<header>
+    <!--Start of navbar code-->
+<header class="header">
+            <!--Code for bakery logo and name in the navbar-->
+            <i class="fa-solid fa-cake-candles fa-3x" style="color: #d3ad7f;"></i>
+            <h1 class="name" style="color: #fff;text-align: left;">THE<span> LITTLE</span> BAKERY</h1>
+            <nav class="navbar">
+                <!--Code for the different links to other pages in the navbar-->
+                <a href="index.php">Home</a>
+                <a href="About.php">About</a>
+                <a href="Menu.php">Menu</a>
+                <a href="Reviews.php">Reviews</a>
+                <a href="contact.php">Contact</a>
+                <a href = "crud.php">Admin</a>
+                <div class="dropdown-container">
+                    <!--Dropdown menu from the link 'More For You'-->
+                    <a href="#" class="dropdown-toggle">More</a>
+                    <ul class="dropdown">
+                        <!--Icons and links to the different pages in the dropdown menu-->
+                        <li><i class="fa-solid fa-right-to-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="login.php">Log In</a></li>
+                        <li><i class="fa-solid fa-user fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="signup.php">Sign Up</a></li>
+                        <li><i class="fa-solid fa-right-from-bracket fa-2x" style="color: #fff; margin-left: 7px;"></i><a href="#">Log Out</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
     <div class="container">
         <div id="branding">
-            <h1>CRUD Application</h1>
+            <h1>View, Edit, Delete</h1>
         </div>
         <nav>
             <ul>
