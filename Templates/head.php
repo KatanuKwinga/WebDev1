8157e9a12f591369cc4a22ca4fc788ee6f700257
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-07-08 20:15:03 +0300

    edited links in header

--- Templates/head.php
+++ Templates/head.php
@@ -5,6 +5,7 @@
         <title>MyWebsite</title>
        <!-- custom css file link -->
         <link rel="stylesheet" href="css/style.css">
+        <link rel = "icon" type = "image/x-icon" href = "images/Logo.png">
         <!--Link to the page for icons used in the code-->
         <script src="https://kit.fontawesome.com/061a76efba.js"crossorigin="anonymous"></script>
     </head>
